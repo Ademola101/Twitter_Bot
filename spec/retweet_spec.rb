1d3412cdee783ff3723aebbb00b7ff3cfbb94662
Ademola <ademolaogunmokun@yahoo.com>
2022-01-06 18:53:40 +0100

    autocorrect rubocop

--- spec/retweet_spec.rb
+++ spec/retweet_spec.rb
@@ -3,7 +3,7 @@ describe 'Retweet' do
   let(:MAX_HASHTAG) { Array.new(12, '#rails') }
   let(:ade) { Twitter::Retweet.new }
   describe '#tweet' do
-    it 'should return false if the class is of a tweet'do
+    it 'should return false if the class is of a tweet' do
       expect(ade.send(:tweet?, 'This is a string and not a tweet')).to eql false
     end
   end
